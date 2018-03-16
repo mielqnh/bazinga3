4a7f8372b34adc1ba7514694d6f00fc523fd1d33
Mampaey Walter <mampaey.walter@telenet.be>
2018-03-16 12:34:13 +0100

    WmPublisherEndPoint added

--- src/main/java/be/mielnoelanders/bazinga/repository/ParameterRepository.java
+++ src/main/java/be/mielnoelanders/bazinga/repository/ParameterRepository.java
@@ -8,6 +8,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ParameterRepository extends JpaRepository<Parameter,Long> {
 
-    // Get parameter by type //
-    Parameter findByType(ParameterEnum type);
 }
--- src/test/java/be/mielnoelanders/bazinga/restcontroller/ParameterEndPointIT.java
+++ src/test/java/be/mielnoelanders/bazinga/restcontroller/ParameterEndPointIT.java
@@ -40,54 +40,74 @@ public class ParameterEndPointIT {
 
     @Test
     public void crudParameterTest() throws JSONException {
+        //create Parameter instance
         Parameter newParm = new Parameter();
         newParm.setType(ParameterEnum.PROMOTIONALDISCOUNT);
         newParm.setPercentage(21);
-        HttpEntity<Parameter> entity = new HttpEntity<>(newParm, httpHeaders);
+
+        // testAddOne()
+        HttpEntity<Parameter> entityAddOne = new HttpEntity<>(newParm, httpHeaders);
         httpHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);
-        ResponseEntity<Parameter> responseEntity = testRestTemplate.postForEntity(createURLWithPort(BASE_URI + "/"), entity, Parameter.class);
-        System.out.println("responseEntity.getBody = " + responseEntity.getBody());
-        System.out.println("HttpStatus = " + responseEntity.getStatusCode().toString());
+        ResponseEntity<Parameter> responseEntityAddOne = testRestTemplate.postForEntity(createURLWithPort(BASE_URI + "/"), entityAddOne, Parameter.class);
 
-        assertThat(responseEntity.getBody()).isNotNull();
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        assertThat(responseEntity.getBody().getType()).isEqualTo(ParameterEnum.PROMOTIONALDISCOUNT);
-        Long newId = responseEntity.getBody().getId();
+        System.out.println("responseEntity.getBody()) = " + responseEntityAddOne.getBody());
+        System.out.println("responseEntity.getStatusCode()) = " + responseEntityAddOne.getStatusCode());
+        assertThat(responseEntityAddOne.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(responseEntityAddOne.getBody().getType()).isEqualTo(ParameterEnum.PROMOTIONALDISCOUNT);
+        Long newId = responseEntityAddOne.getBody().getId();
 
-        entity = new HttpEntity<>(null, httpHeaders);
-        ResponseEntity<String> response = testRestTemplate.exchange(createURLWithPort(BASE_URI + "/findall"),
-        HttpMethod.GET, entity, String.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        // test findall() String
+        HttpEntity<Parameter> entity = new HttpEntity<>(null, httpHeaders);
+        ResponseEntity<String> response = testRestTemplate.exchange(createURLWithPort(BASE_URI + "/findall"),HttpMethod.GET, entity, String.class);
 
-        System.out.println("response = " + response.getBody());
+        System.out.println("responseEntity.getBody()) = " + response.getBody());
+        System.out.println("responseEntity.getStatusCode()) = " + response.getStatusCode());
+        assertThat(response.getStatusCode()).isEqualTo( HttpStatus.OK);
         String expected = "[{\"id\":1,\"type\":\"PROFITMARGIN\",\"percentage\":30},{\"id\":2,\"type\":\"PREMIUMCUSTOMER\",\"percentage\":10},{\"id\":3,\"type\":\"DAMAGEDISCOUNT\",\"percentage\":20},{\"id\":4,\"type\":\"PROMOTIONALDISCOUNT\",\"percentage\":21}]";
         JSONAssert.assertEquals(expected, response.getBody(), false);
 
+        // test findall() Iterable
         ResponseEntity<Iterable> iterableResponseEntity = testRestTemplate.getForEntity(createURLWithPort(BASE_URI + "/findall"), Iterable.class);
-        System.out.println("HttpStatus = " + iterableResponseEntity.getStatusCode().toString());
+
+        System.out.println("responseEntity.getBody()) = " + iterableResponseEntity.getBody());
+        System.out.println("responseEntity.getStatusCode()) = " + iterableResponseEntity.getStatusCode());
         assertThat(iterableResponseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat((List) iterableResponseEntity.getBody()).size().isEqualTo(4);
 
-        //find Parameter with type=PROFITMARGIN that exists
+        //findById that exists
         ResponseEntity<Parameter> responseEntityOK = testRestTemplate.getForEntity(createURLWithPort(BASE_URI + "/" + newId), Parameter.class);
-        System.out.println("HttpStatus = " + responseEntityOK.getStatusCode().toString());
-        assertThat(responseEntityOK.getBody()).isNotNull();
+
+        System.out.println("responseEntity.getBody()) = " + responseEntityOK.getBody());
+        System.out.println("responseEntity.getStatusCode()) = " + responseEntityOK.getStatusCode());
         assertThat(responseEntityOK.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(responseEntityOK.getBody().getType()).isEqualTo(ParameterEnum.PROMOTIONALDISCOUNT);
 
-        //delete Parameter with id=1 that exists
+        //deleteOneById with id=1 that exists
         entity = new HttpEntity<>(null, httpHeaders);
-        ResponseEntity<String> responseDelete = testRestTemplate.exchange(createURLWithPort(BASE_URI + "/1"),
-        HttpMethod.DELETE, entity, String.class);
-        System.out.println("HttpStatus = " + responseDelete.getStatusCode().toString());
+        ResponseEntity<Boolean> responseDelete = testRestTemplate.exchange(createURLWithPort(BASE_URI + "/1"),HttpMethod.DELETE, entity, Boolean.class);
+
+        System.out.println("responseEntity.getBody()) = " + responseDelete.getBody());
+        System.out.println("responseEntity.getStatusCode()) = " + responseDelete.getStatusCode());
         assertThat(responseDelete.getStatusCode()).isEqualTo(HttpStatus.OK);
 
-        //try to lookup Parameter with type=PROFITMARGIN which is deleted
+        //findById which is deleted
         ResponseEntity<Parameter> responseEntityFind = testRestTemplate.getForEntity(createURLWithPort(BASE_URI + "/1"), Parameter.class);
-        System.out.println("HttpStatus = " + responseEntityFind.getStatusCode().toString());
-//        assertThat(responseEntityFind.getBody()).isNull();
+
+        System.out.println("responseEntity.getBody()) = " + responseEntityFind.getBody());
+        System.out.println("responseEntity.getStatusCode()) = " + responseEntityFind.getStatusCode());
         assertThat(responseEntityFind.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
+
+//    private void checkBodyAndHttpStatusResponseEntity(ResponseEntity responseEntity, int responseBodyValue, HttpStatus httpStatus) {
+//        System.out.println("responseEntity.getBody()) = " + responseEntity.getBody());
+//        System.out.println("responseEntity.getStatusCode()) = " + responseEntity.getStatusCode());
+//        if (responseBodyValue == 0) {
+//            assertThat(responseEntity.getBody()).isNull();
+//        } else {
+//            assertThat(responseEntity.getBody()).isNotNull();
+//        }
+//        assertThat(responseEntity.getStatusCode()).isEqualTo(httpStatus);
+//    }
 
     private String createURLWithPort(String uri) {
         String uriString = "http://localhost:" + port + uri;
